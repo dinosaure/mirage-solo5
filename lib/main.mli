cc8fe082ffb03d7b61a0a41e33f6e07a74625fc3
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-10-10 16:51:39 +0200

    Delete faked wait_for_work and externalize wait_for_work_on_handle

--- lib/main.mli
+++ lib/main.mli
@@ -1,4 +1,4 @@
-val wait_for_work : unit -> unit Lwt.t
+val wait_for_work_on_handle : int64 -> unit Lwt.t
 val run : unit Lwt.t -> unit
 val at_enter : (unit -> unit Lwt.t) -> unit
 val at_enter_iter : (unit -> unit) -> unit
